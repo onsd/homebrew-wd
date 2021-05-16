7b63a0889f6c90ce65768e206a62aebea901540c
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-16 13:10:46 +0900

    Brew formula update for wd version v1.3.0

--- wd.rb
+++ wd.rb
@@ -5,20 +5,20 @@
 class Wd < Formula
   desc ""
   homepage ""
-  version "1.2.0"
+  version "1.3.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/onsd/wd/releases/download/v1.2.0/wd_1.2.0_darwin_amd64.tar.gz"
-    sha256 "70f0ee8cbfe7732b410f184d16289406a5176f0fe9ff61bd42ca298ac7d23d43"
+    url "https://github.com/onsd/wd/releases/download/v1.3.0/wd_1.3.0_darwin_amd64.tar.gz"
+    sha256 "2048b5474ccb868fb4b192c050be73f3fe9b6bc6139109b62a115258cfae7ecd"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/onsd/wd/releases/download/v1.2.0/wd_1.2.0_linux_amd64.tar.gz"
-    sha256 "9665faeceabe926c6d41b2932e915082d79ebbe0e16bc22d3573de98d616b13f"
+    url "https://github.com/onsd/wd/releases/download/v1.3.0/wd_1.3.0_linux_amd64.tar.gz"
+    sha256 "0683bf9d41b0f2c5f083267fef92f9aa37833d68e30a2eb68bd425fc61973440"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/onsd/wd/releases/download/v1.2.0/wd_1.2.0_linux_arm64.tar.gz"
-    sha256 "08063730f24ccac3f5cf41e2b99b44d3d6513bd87daa9e404d879ba300270ca9"
+    url "https://github.com/onsd/wd/releases/download/v1.3.0/wd_1.3.0_linux_arm64.tar.gz"
+    sha256 "bdf5f354f544da550cc4324d5428437ea7c76f82364587a3b93d9d23e6d08ef2"
   end
 
   def install
